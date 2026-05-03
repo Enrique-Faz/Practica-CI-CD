3a122d84614d312e4725ac5f2775fa685081b91a
Enrique_DAW <enrique71296@gmail.com>
2026-05-03 11:55:01 +0200

    Dockerizacion de backend

--- backend/dnd-board/routes/api.php
+++ backend/dnd-board/routes/api.php
@@ -9,9 +9,17 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('auth/{provider}', [AuthController::class, 'redirectToProvider']);
+Route::get('auth/{provider}/callback', [AuthController::class, 'handleProviderCallback']);
+
+Route::post('/verify-2fa', [AuthController::class, 'verify2fa']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']); // Para obtener los datos de mi perfil
+    Route::get('/user', [AuthController::class, 'user']);
+
+    Route::post('/2fa/generate', [AuthController::class, 'generate2faSecret']);
+    Route::post('/2fa/enable', [AuthController::class, 'enable2fa']);
 
     Route::apiResource('characters', CharacterController::class);
 
