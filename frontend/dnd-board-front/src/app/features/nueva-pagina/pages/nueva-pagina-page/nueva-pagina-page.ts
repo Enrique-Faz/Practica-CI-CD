68063d50b52a4c53b9a3755dff842808168d19f4
Enrique_Faz <efazdio605@g.educaand.es>
2026-05-24 16:42:14 +0200

    feat: new placeholder page (#3)

---
+++ frontend/dnd-board-front/src/app/features/nueva-pagina/pages/nueva-pagina-page/nueva-pagina-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-pagina-page',
+  imports: [],
+  templateUrl: './nueva-pagina-page.html',
+  styleUrl: './nueva-pagina-page.css',
+})
+export class NuevaPaginaPage {}
